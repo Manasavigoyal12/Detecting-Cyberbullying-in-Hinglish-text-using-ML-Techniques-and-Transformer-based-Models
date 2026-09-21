a1445baf24a3eebd0068a784bbaf58c06e716b81
Manasavi Goyal <166170564+Manasavigoyal12@users.noreply.github.com>
2026-09-21 22:46:28 +0530

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import re
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix
+)
